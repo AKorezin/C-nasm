9b03e9944461efb07b9ed6844c84f85cb8bff9bd
AKorezin <source-email@yandex.ru>
2017-09-19 01:39:27 +0300

    Testing execution time

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #define N 50
 
 int add(int l, int *a, int *b){
@@ -16,12 +17,25 @@ int main(int argc, char** argv){
 	int l=N;
 	int *a=(int*)malloc(l*sizeof(int));
 	int *b=(int*)malloc(l*sizeof(int));
+
+
 	for(int i=0;i<l;i++){
 		a[i]=i;
 		b[i]=i;
 	}
-	printf("%d\n",add(l,a,b));
-	printf("%d\n",add_asm(l,a,b));
+
+	clock_t begin = clock();
+	for(int i=0;i<1000000;i++)
+		add(l,a,b);
+	clock_t end = clock();
+	int time_spent = end - begin;
+	printf("%d\n",time_spent);
+	begin = clock();
+	for(int i=0;i<1000000;i++)
+		add_asm(l,a,b);
+	end = clock();
+	time_spent = end - begin;
+	printf("%d\n",time_spent);
 
 	free(a);
 	free(b);
