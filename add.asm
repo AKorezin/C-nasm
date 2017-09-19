3b08e2b7883b21310d35a82978f0fe3a1a5bfe14
AKorezin <source-email@yandex.ru>
2017-09-19 19:59:15 +0300

    More speed

--- add.asm
+++ add.asm
@@ -2,16 +2,17 @@ global add_asm
 section .text
 
 add_asm:
-	mov rax,0
-check:
+	xor eax,eax
 	test edi, edi
 	jz done
-	mov r10, [rdx]
-	imul r10, [rsi]
-	add rax, r10
-	add rdx,4
-	add rsi,4
-	dec edi
-	jmp check
+	lea r9, [edi*4]
+	xor r11, r11
+loop:
+	mov r10d, [rdx+r11]
+	imul r10d, [rsi+r11]
+	add eax, r10d
+	add r11, 4
+	cmp r11, r9
+	jne loop
 done:
 	ret
--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#define N 1000
+#define N 50
 
 int add(int l, int *a, int *b){
 	int s=0;
@@ -24,15 +24,19 @@ int main(int argc, char** argv){
 		b[i]=i;
 	}
 	volatile int test;
-	printf("%d\n",sizeof(int));
 	printf("%d\n",add(l,a,b));
 	printf("%d\n",add_asm(l,a,b));
-	clock_t begin = clock();
+	clock_t begin=0,end=0;
+	int time_spent=0;
+	begin = clock();
 	for(int i=0;i<1000000;i++)
 		test=add(l,a,b);
-	clock_t end = clock();
-	int time_spent = end - begin;
+	end = clock();
+	time_spent = end - begin;
 	printf("%d\n",time_spent);
+	begin=0;
+	end=0;
+	time_spent=0;
 	begin = clock();
 	for(int i=0;i<1000000;i++)
 		test=add_asm(l,a,b);
