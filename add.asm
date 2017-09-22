0b8f09444e6da3702d5eb2cfb34b0c8e66d910c8
AKorezin <source-email@yandex.ru>
2017-09-22 03:56:08 +0300

    perfetch and nasm optimization

--- add.asm
+++ add.asm
@@ -7,11 +7,13 @@ add_asm:
 	jz done
 	lea r9, [edi*4]
 	xor r11, r11
+	prefetchnta [rdx+r11+128]
+	prefetchnta [rsi+r11+128]
 loop:
 	mov r10d, [rdx+r11]
 	imul r10d, [rsi+r11]
-	add eax, r10d
 	add r11, 4
+	add eax, r10d
 	cmp r11, r9
 	jne loop
 done:
